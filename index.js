85130e392de02565295478e96abb78a14ca921a9
John Bedette <jbedette@yahoo.com>
2017-08-21 11:44:20 -0400

    got index view working

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'pug');
 
-app.get('/', function(req,res) {
-  res.send('oh shit waddup');
-});
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hi there!' })
+})
 
 app.listen(3000,function () {
   console.log('example app listening on port 3000!');
