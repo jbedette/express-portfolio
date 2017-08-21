92669d17ac05e81acdbba3a8799709f1cc0dbc84
John Bedette <jbedette@yahoo.com>
2017-08-21 14:14:10 -0400

    css work

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hi there!' })
